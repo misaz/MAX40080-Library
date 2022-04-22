b56ca83b215a40e3995b61914b7d4405f9f906a0
Michal Žůrek <zurmich@volny.cz>
2022-04-23 00:22:36 +0200

    First example for MAX32625 MCU platform.

---
+++ examples/01_read_current_continous/README.md
@@ -0,0 +1,93 @@
+# Exmaple 01: SImple Current Measurements
+This example shows basic usage of MAX40080 CSA sensor. It uses almost standard configuration of MAX40080 sensor. Only change is that change mode from Standby to Active.
+
+Code does following tasks:
+
+1. Init the library
+2. Configure sensor
+3. Read current samles in infinite loop
+4. Print samples over UART
+
+## Initializing library
+Library initialization is simple. Library can be initalized using following code snippet:
+
+```
+status = MAX40080_Init();
+if (status) {
+	// handle error
+}
+```
+
+## Configure sensor
+Sensor is configured using `MAX40080_SetConfiguration` function. This functions requires pointer to the `MAX40080_Configuration`. You can fill the structure manualy or you can use `MAX40080_GetDefaultConfiguration` function for preparing structure and then you can just modify it partially.
+
+Example code use default values and modifies only `operatingMode` field of structure.
+
+```
+MAX40080_Configuration config;
+MAX40080_GetDefaultConfiguration(&config);
+config.operatingMode = MAX40080_OperationMode_Active;
+```
+
+Equaivalent code with manualy filling whole structure is:
+
+```
+MAX40080_Configuration config;
+config->operatingMode = MAX40080_OperationMode_Active;
+config->i2cTimeoutSettings = MAX40080_I2CTimeoutSettings_Enabled;
+config->alertResponseTime = MAX40080_AlertResponseTime_Unfiltered;
+config->packetErrorChecking = MAX40080_PacketErrorChecking_Enabled;
+config->inputRange = MAX40080_InputRange_10mV;
+config->stayHsMode = MAX40080_StayHsMode_ExitAtStop;
+config->adcSampleRate = MAX40080_AdcSampleRate_Either_at_15_ksps;
+config->digitalFilter = MAX40080_DigitalFilter_NoAverage;
+```
+
+After you have prepared configuration structure, you can pass it to the `MAX40080_SetConfiguration` function:
+
+```
+status = MAX40080_SetConfiguration(&config);
+if (status) {
+	// handle error
+}
+```
+
+## Reading samples from sensor
+
+Reading measured sample can be done using `MAX40080_ReadCurrent`. If you call the function before the collection is complete, then function returns status code `MAX40080_Status_FifoIsEmpty`. For retrieving number of samples in the FIFO you can use function `MAX40080_GetAvailableFifoDataCount` but because sensor is fast, another transaction caused by `MAX40080_GetAvailableFifoDataCount` can reduce the overal performance. Usualy is better to just try read the sample and discard them in case of detecting empty FIFO.
+
+```
+while (1) {
+	float current;
+
+	status = MAX40080_ReadCurrent(&current);
+	if (status && status != MAX40080_Status_FifoIsEmpty) {
+		// handle error
+		continue;
+	}
+
+	if (status == MAX40080_Status_FifoIsEmpty) {
+		continue;
+	}
+
+	// print current over UART
+}
+```
+
+# Configurable constans
+Library and this example assumes using with MikroE Current 6 Click board. If you use other or custom board, for correct operation you need to edit `MAX40080_I2C_7BIT_ADDRESS` and `MAX40080_SHUNT_RESISOTR_VALUE` constants in `MAX40080.h` file. I2C address depends on value of resistor connected to A0 pin. Consult I2C Slave Addresses table in datasheet for getting address number coresponding to your resistor. Both MikroE Current 6 Click Board and MAX40080EVKIT uses 100kOhm resisotr which coresponds to address 0x21. Similarly you need to change values of shunt resistor as a float value in ohms. MikroeE Current 6 Click Board uses 10mOhm (0.010 ohms) and this value is by default in source code. MAX40080EVKIT uses shunt resistor of value 0.050 ohms and if you use this board you need to update `MAX40080_SHUNT_RESISOTR_VALUE` constant to this value.
+
+# MAX32625 port
+This example is implemented for MAX32625 MCU targetting MAX32625PICO board and MikroE Current 6 Click Board containing MAX40080 sensor. Folder (max32625)[max32625] contains Eclipse project created using `MAX32625PeriphDriver` from [Low Power Microcontroller SDK](https://www.maximintegrated.com/en/design/software-description.html/swpart=SFW0001500A).
+
+> :warning: Implementation use 3.3V voltage level. MikroE Current 6 Click Board has onboard level shifter. If you use different board like MAX40080EVKIT, then you need to modify the `MAX40080_PlatformSpecific.c` file and remove TWO `SYS_IOMAN_UseVDDIOH(&gpio);` lines from `MAX40080_PlatformSpecific_Init`.
+
+For deploying and debugging directly from Eclipse you will need to define Launch configuration. Use the same Launch configuration which you use for launching examples from MAX32625 SDK. For debugging use `GDB OpenOCD Debugging`, C/C++ Application set to `build\max32625.elf`. At `Debugger` tab set Executable to `C:\Maxim\Toolchain\bin\openocd.exe`, GDB port 3333, telnet port 4444 and config options set to:
+
+```
+-s C:\Maxim\Toolchain\share\openocd\scripts -f "interface/{name_of_script_file_related_to_your_debug_probe}.cfg" -f target\max32625.cfg
+```
+
+For example:
+
+![Configuration of Eclipse launch profile](readme_assets/max32625_debug_cfg.png)
---
+++ examples/01_read_current_continous/max32625/MAX40080.h
@@ -0,0 +1,272 @@
+#ifndef __MAX40080_H
+#define __MAX40080_H
+
+#include <stdint.h>
+
+// default 7-bit I2C address 0x21 is valid when A0 pin is connected using 100k resistor to ground. 
+// If you use different resistor, edit address here or use -D compiler parameter.
+#ifndef MAX40080_I2C_7BIT_ADDRESS
+#define MAX40080_I2C_7BIT_ADDRESS					0x21
+#endif
+
+// default resistor value is 10mOhm. If you use different resistor, then edit value here or use
+// -D compiler parameter
+#ifndef MAX40080_SHUNT_RESISOTR_VALUE
+#define MAX40080_SHUNT_RESISOTR_VALUE				0.010
+#endif
+
+#define MAX40080_I2C_READ_ADDRESS					((MAX40080_I2C_7BIT_ADDRESS << 1) | 0x01)
+#define MAX40080_I2C_WRITE_ADDRESS					((MAX40080_I2C_7BIT_ADDRESS << 1) | 0x00)
+
+#define MAX40080_REG_CONFIG							0x00
+#define MAX40080_REG_STATUS							0x02
+#define MAX40080_REG_TRESHOLD_OVER_CURRENT			0x04
+#define MAX40080_REG_TRESHOLD_OVER_VOLTAGE			0x05
+#define MAX40080_REG_TRESHOLD_UNDER_VOLTAGE			0x06
+#define MAX40080_REG_WAKE_UP_CURRENT				0x07
+#define MAX40080_REG_MAX_PEAK_CURRENT				0x08
+#define MAX40080_REG_FIFO_CONFIG					0x0A
+#define MAX40080_REG_CURRENT_MEASUREMENT			0x0C
+#define MAX40080_REG_VOLTAGE_MEASUREMENT			0x0E
+#define MAX40080_REG_CURRENT_VOLTAGE_MEASUREMENT	0x10
+#define MAX40080_REG_INTERRUPT_ENABLE				0x14
+
+#define MAX40080_EXPAND_MACRO(content)				content
+#define MAX40080_FIELD(NAME)						NAME##_MASK, NAME##_OFFSET
+#define MAX40080_GET_FIELD_IMPL(FIELD_MASK,FIELD_OFFSET,value)	(((value) >> FIELD_OFFSET) & FIELD_MASK)
+#define MAX40080_SET_FIELD_IMPL(FIELD_MASK,FIELD_OFFSET,value)	(((value) & FIELD_MASK) << FIELD_OFFSET)
+#define MAX40080_GET_FIELD(...)						MAX40080_EXPAND_MACRO(MAX40080_GET_FIELD_IMPL(__VA_ARGS__))
+#define MAX40080_SET_FIELD(...)						MAX40080_EXPAND_MACRO(MAX40080_SET_FIELD_IMPL(__VA_ARGS__))
+
+#define MAX40080_CONFIG_MODE_MASK					0x7
+#define MAX40080_CONFIG_MODE_OFFSET					0
+#define MAX40080_CONFIG_MODE_FIELD					MAX40080_FIELD(MAX40080_CONFIG_MODE)
+#define MAX40080_CONFIG_I2C_TIMEOUT_MASK			0x1
+#define MAX40080_CONFIG_I2C_TIMEOUT_OFFSET			3
+#define MAX40080_CONFIG_I2C_TIMEOUT_FIELD			MAX40080_FIELD(MAX40080_CONFIG_I2C_TIMEOUT)
+#define MAX40080_CONFIG_ALERT_MASK					0x1
+#define MAX40080_CONFIG_ALERT_OFFSET				4
+#define MAX40080_CONFIG_ALERT_FIELD					MAX40080_FIELD(MAX40080_CONFIG_ALERT)
+#define MAX40080_CONFIG_PEC_MASK					0x1
+#define MAX40080_CONFIG_PEC_OFFSET					5
+#define MAX40080_CONFIG_PEC_FIELD					MAX40080_FIELD(MAX40080_CONFIG_PEC)
+#define MAX40080_CONFIG_INPUT_RANGE_MASK			0x1
+#define MAX40080_CONFIG_INPUT_RANGE_OFFSET			6
+#define MAX40080_CONFIG_INPUT_RANGE_FIELD			MAX40080_FIELD(MAX40080_CONFIG_INPUT_RANGE)
+#define MAX40080_CONFIG_STAY_HS_MODE_MASK			0x1
+#define MAX40080_CONFIG_STAY_HS_MODE_OFFSET			7
+#define MAX40080_CONFIG_STAY_HS_MODE_FIELD			MAX40080_FIELD(MAX40080_CONFIG_STAY_HS_MODE)
+#define MAX40080_CONFIG_ADC_SAMPLE_RATE_MASK		0xF
+#define MAX40080_CONFIG_ADC_SAMPLE_RATE_OFFSET		8
+#define MAX40080_CONFIG_ADC_SAMPLE_RATE_FIELD		MAX40080_FIELD(MAX40080_CONFIG_ADC_SAMPLE_RATE)
+#define MAX40080_CONFIG_DIGITAL_FILTER_MASK			0x7
+#define MAX40080_CONFIG_DIGITAL_FILTER_OFFSET		12
+#define MAX40080_CONFIG_DIGITAL_FILTER_FIELD		MAX40080_FIELD(MAX40080_CONFIG_DIGITAL_FILTER)
+
+#define MAX40080_STATUS_FIFO_DATA_COUNT_MASK		0x3F
+#define MAX40080_STATUS_FIFO_DATA_COUNT_OFFSET		8
+#define MAX40080_STATUS_FIFO_DATA_COUNT_FIELD		MAX40080_FIELD(MAX40080_STATUS_FIFO_DATA_COUNT)
+
+#define MAX40080_FIFO_CONFIG_STORING_MODE_MASK		0x3
+#define MAX40080_FIFO_CONFIG_STORING_MODE_OFFSET	0
+#define MAX40080_FIFO_CONFIG_STORING_MODE_FIELD		MAX40080_FIELD(MAX40080_FIFO_CONFIG_STORING_MODE) 
+#define MAX40080_FIFO_CONFIG_WARNING_LEVEL_MASK		0x3F
+#define MAX40080_FIFO_CONFIG_WARNING_LEVEL_OFFSET	8
+#define MAX40080_FIFO_CONFIG_WARNING_LEVEL_FIELD	MAX40080_FIELD(MAX40080_FIFO_CONFIG_WARNING_LEVEL) 
+#define MAX40080_FIFO_CONFIG_ROLL_OVER_MASK			0x1
+#define MAX40080_FIFO_CONFIG_ROLL_OVER_OFFSET		14
+#define MAX40080_FIFO_CONFIG_ROLL_OVER_FIELD		MAX40080_FIELD(MAX40080_FIFO_CONFIG_ROLL_OVER) 
+#define MAX40080_FIFO_CONFIG_FLUSH_MASK				0x1
+#define MAX40080_FIFO_CONFIG_FLUSH_OFFSET			15
+#define MAX40080_FIFO_CONFIG_FLUSH_FIELD			MAX40080_FIELD(MAX40080_FIFO_CONFIG_FLUSH) 
+
+#define MAX40080_FIFO_CAPACITY						64
+
+typedef enum {
+	MAX40080_Status_Ok = 0,
+	MAX40080_Status_I2CNack = -10,
+	MAX40080_Status_I2CError = -11,
+	MAX40080_Status_I2CTimeout = -12,
+	MAX40080_Status_PacketErrorCheckFailed = -13,
+	MAX40080_Status_NotImplemented = -20,
+	MAX40080_Status_BadArg = -21,
+	MAX40080_Status_InvalidOperation = -22,
+	MAX40080_Status_FifoIsEmpty = -23,
+} MAX40080_Status;
+
+typedef enum {
+	MAX40080_OperationMode_Standby = 0,
+	MAX40080_OperationMode_LowPower = 1,
+	MAX40080_OperationMode_SingleConversion = 2,
+	MAX40080_OperationMode_Active = 3,
+	MAX40080_OperationMode_SelectedActive_4_SPS = 4,
+	MAX40080_OperationMode_SelectedActive_1_SPS = 5,
+	MAX40080_OperationMode_SelectedActive_0_25_SPS = 6,
+	MAX40080_OperationMode_SelectedActive_0_0625_SPS = 7
+} MAX40080_OperationMode;
+
+typedef enum {
+	MAX40080_I2CTimeoutSettings_Enabled = 0,
+	MAX40080_I2CTimeoutSettings_Disabled = 1,
+} MAX40080_I2CTimeoutSettings;
+
+typedef enum {
+	MAX40080_AlertResponseTime_Unfiltered = 0,
+	MAX40080_AlertResponseTime_FourConsecutive = 1,
+} MAX40080_AlertResponseTime;
+
+typedef enum {
+	MAX40080_PacketErrorChecking_Disabled = 0,
+	MAX40080_PacketErrorChecking_Enabled = 1,
+} MAX40080_PacketErrorChecking;
+
+typedef enum {
+	MAX40080_InputRange_50mV = 0,
+	MAX40080_InputRange_10mV = 1,
+} MAX40080_InputRange;
+
+typedef enum {
+	MAX40080_StayHsMode_ExitAtStop = 0,
+	MAX40080_StayHsMode_Stay = 1,
+} MAX40080_StayHsMode;
+
+typedef enum {
+	MAX40080_AdcSampleRate_Either_at_15_ksps = 0,
+	MAX40080_AdcSampleRate_Either_at_18_75_ksps = 1,
+	MAX40080_AdcSampleRate_Either_at_23_45_ksps = 2,
+	MAX40080_AdcSampleRate_Either_at_30_ksps = 3,
+	MAX40080_AdcSampleRate_Either_at_37_5_ksps = 4,
+	MAX40080_AdcSampleRate_Either_at_47_1_ksps = 5,
+	MAX40080_AdcSampleRate_Either_at_60_ksps = 6,
+	MAX40080_AdcSampleRate_Either_at_93_5_ksps = 7,
+	MAX40080_AdcSampleRate_Either_at_120_ksps = 8,
+	MAX40080_AdcSampleRate_Either_at_150_ksps = 9,
+	MAX40080_AdcSampleRate_Either_at_234_5_ksps = 10,
+	MAX40080_AdcSampleRate_Either_at_375_ksps = 11,
+	MAX40080_AdcSampleRate_Either_at_468_5_ksps = 12,
+	MAX40080_AdcSampleRate_Either_at_750_ksps = 13,
+	MAX40080_AdcSampleRate_Either_at_1000_ksps = 14,
+	MAX40080_AdcSampleRate_Both_at_0_5_ksps= 15,
+} MAX40080_AdcSampleRate;
+
+typedef enum {
+	MAX40080_DigitalFilter_NoAverage = 0,
+	MAX40080_DigitalFilter_8_samples = 1,
+	MAX40080_DigitalFilter_16_samples = 2,
+	MAX40080_DigitalFilter_32_samples = 3,
+	MAX40080_DigitalFilter_64_samples = 4,
+	MAX40080_DigitalFilter_128_samples = 5
+} MAX40080_DigitalFilter;
+
+typedef enum {
+	MAX40080_FifoStoringMode_CurrentOnly = 0,
+	MAX40080_FifoStoringMode_VoltageOnly = 1,
+	MAX40080_FifoStoringMode_CurrentAndVoltage = 2
+} MAX40080_FifoStoringMode;
+
+typedef enum {
+	MAX40080_FifoRollOverMode_Ignore = 0,
+	MAX40080_FifoRollOverMode_OverwriteFirst = 1
+} MAX40080_FifoRollOverMode;
+
+typedef enum {
+	MAX40080_Interrupt_WakeUp = 1,
+	MAX40080_Interrupt_ConversionReady = 2,
+	MAX40080_Interrupt_CurrentOverflow = 4,
+	MAX40080_Interrupt_VoltageOverflow = 8,
+	MAX40080_Interrupt_VoltageUnderflow = 16,
+	MAX40080_Interrupt_I2CInactive = 32,
+	MAX40080_Interrupt_FifoOverflowWarning = 64,
+	MAX40080_Interrupt_FifoOverflown = 128
+} MAX40080_Interrupt;
+
+typedef struct {
+	MAX40080_OperationMode operatingMode;
+	MAX40080_I2CTimeoutSettings i2cTimeoutSettings;
+	MAX40080_AlertResponseTime alertResponseTime;
+	MAX40080_PacketErrorChecking packetErrorChecking;
+	MAX40080_InputRange inputRange;
+	MAX40080_StayHsMode stayHsMode;
+	MAX40080_AdcSampleRate adcSampleRate;
+	MAX40080_DigitalFilter digitalFilter;
+} MAX40080_Configuration;
+
+typedef struct {
+	MAX40080_FifoStoringMode storingMode;
+	MAX40080_FifoRollOverMode rollOverMode;
+	uint8_t overflowWarningLevel;
+} MAX40080_FifoConfiguration;
+
+MAX40080_Status MAX40080_Init();
+MAX40080_Status MAX40080_Deinit();
+
+MAX40080_Status MAX40080_GetDefaultConfiguration(MAX40080_Configuration* config);
+MAX40080_Status MAX40080_GetConfiguration(MAX40080_Configuration* config);
+MAX40080_Status MAX40080_SetConfiguration(MAX40080_Configuration* config);
+
+MAX40080_Status MAX40080_EnableInterrupts(MAX40080_Interrupt interrupts);
+MAX40080_Status MAX40080_GetPendingInterrupts(MAX40080_Interrupt* interrupts);
+MAX40080_Status MAX40080_ClearPendingInterrupts(MAX40080_Interrupt interrupts);
+
+MAX40080_Status MAX40080_GetRawOverCurrentTreshold(uint8_t* treshold);
+MAX40080_Status MAX40080_SetRawOverCurrentTreshold(uint8_t treshold);
+MAX40080_Status MAX40080_GetRawOverVoltageTreshold(uint8_t* treshold);
+MAX40080_Status MAX40080_SetRawOverVoltageTreshold(uint8_t treshold);
+MAX40080_Status MAX40080_GetRawUnderVoltageTreshold(uint8_t* treshold);
+MAX40080_Status MAX40080_SetRawUnderVoltageTreshold(uint8_t treshold);
+MAX40080_Status MAX40080_GetRawWakeUpCurrent(uint8_t* wakeUpCurrent);
+MAX40080_Status MAX40080_SetRawWakeUpCurrent(uint8_t wakeUpCurrent);
+MAX40080_Status MAX40080_GetRawMaxPeakCurrent(uint16_t* maxPeakCurrent);
+
+MAX40080_Status MAX40080_GetFifoDefaultConfiguration(MAX40080_FifoConfiguration* fifoConfig);
+MAX40080_Status MAX40080_GetFifoConfiguration(MAX40080_FifoConfiguration* fifoConfig);
+MAX40080_Status MAX40080_SetFifoConfiguration(MAX40080_FifoConfiguration* fifoConfig);
+MAX40080_Status MAX40080_GetAvailableFifoDataCount(uint8_t* fifoDataCount);
+MAX40080_Status MAX40080_FlushFifo();
+
+MAX40080_Status MAX40080_ReadRawCurrent(int16_t* current);
+MAX40080_Status MAX40080_ReadRawVoltage(int16_t* voltage);
+MAX40080_Status MAX40080_ReadRawCurrentAndVoltage(int16_t* current, int16_t* voltage);
+
+MAX40080_Status MAX40080_ReadCurrent(float* current);
+MAX40080_Status MAX40080_ReadVoltage(float* voltage);
+MAX40080_Status MAX40080_ReadCurrentAndVoltage(float* current, float* voltage);
+
+// Use MAX40080_SetConfiguration and MAX40080_GetConfiguration for access following features:
+//MAX40080_Status MAX40080_GetOperationMode(MAX40080_OperationMode* opMode);
+//MAX40080_Status MAX40080_SetOperationMode(MAX40080_OperationMode opMode);
+//MAX40080_Status MAX40080_GetI2CTimeoutSettings(MAX40080_I2CTimeoutSettings* timeoutSettings);
+//MAX40080_Status MAX40080_SetI2CTimeoutSettings(MAX40080_I2CTimeoutSettings timeoutSettings);
+//MAX40080_Status MAX40080_GetAlertResponseTime(MAX40080_AlertResponseTime* alertResponseTime);
+//MAX40080_Status MAX40080_SetAlertResponseTime(MAX40080_AlertResponseTime alertResponseTime);
+//MAX40080_Status MAX40080_GetPacketErrorChecking(MAX40080_PacketErrorChecking* pec);
+//MAX40080_Status MAX40080_SetPacketErrorChecking(MAX40080_PacketErrorChecking* pec);
+//MAX40080_Status MAX40080_GetInputRange(MAX40080_InputRange* inputRange);
+//MAX40080_Status MAX40080_SetInputRange(MAX40080_InputRange inputRange);
+//MAX40080_Status MAX40080_GetStayHsMode(MAX40080_StayHsMode* stayHsMode);
+//MAX40080_Status MAX40080_SetStayHsMode(MAX40080_StayHsMode stayHsMode);
+//MAX40080_Status MAX40080_GetAdcSampleRate(MAX40080_AdcSampleRate* sampleRate);
+//MAX40080_Status MAX40080_SetAdcSampleRate(MAX40080_AdcSampleRate sampleRate);
+//MAX40080_Status MAX40080_GetDigitalFilter(MAX40080_DigitalFilter* digitalFilter);
+//MAX40080_Status MAX40080_SetDigitalFilter(MAX40080_DigitalFilter digitalFilter);
+
+// Use MAX40080_GetPendingInterrupts for access following features:
+//MAX40080_Status MAX40080_IsWakeUpCurrentReached(int* isWakeUpCurrentReached);
+//MAX40080_Status MAX40080_IsConversionReady(int* isConversionReady);
+//MAX40080_Status MAX40080_IsOverCurrent(int* isOverCurrent);
+//MAX40080_Status MAX40080_IsOverVoltage(int* isOverVoltage);
+//MAX40080_Status MAX40080_IsUnderVoltage(int* isUnderVoltage);
+//MAX40080_Status MAX40080_IsI2CInactiveForMinute(int* isI2CInactive);
+//MAX40080_Status MAX40080_IsFifoOverflowWarningActive(int* isFifoTresholdReached);
+//MAX40080_Status MAX40080_IsFifoOverflown(int* isFifoOverflown);
+
+// Use MAX40080_SetFifoConfiguration and MAX40080_GetFifoConfiguration for access following features:
+//MAX40080_Status MAX40080_GetFifoStoringMode(MAX40080_FifoStoringMode* storingMode);
+//MAX40080_Status MAX40080_SetFifoStoringMode(MAX40080_FifoStoringMode storingMode);
+//MAX40080_Status MAX40080_GetFifoOverflowWarningLevel(size_t* fifoOverflowWarningLevel);
+//MAX40080_Status MAX40080_SetFifoOverflowWarningLevel(size_t fifoOverflowWarningLevel);
+//MAX40080_Status MAX40080_GetFifoRollOverMode(MAX40080_FifoRollOverMode* fifoOverflowWarningLevel);
+//MAX40080_Status MAX40080_SetFifoRollOverMode(MAX40080_FifoRollOverMode fifoOverflowWarningLevel);
+
+
+#endif
